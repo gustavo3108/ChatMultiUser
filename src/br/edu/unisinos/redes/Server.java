b541e65857aa12ef8d4d43d857ac67be22a64a08
Gustavo <gustavo-3108@hotmail.com>
2023-04-20 13:51:39 -0300

    Initial commit

---
+++ src/br/edu/unisinos/redes/Server.java
@@ -0,0 +1,45 @@
+package br.edu.unisinos.redes;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                System.out.println("O cliente " + clientHandler.getClientUserName() + " se conectou!");
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        System.out.println("SERVER Online");
+        server.startServer();
+
+    }
+}
